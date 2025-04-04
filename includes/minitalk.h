5548d5ebd5a59f9c554bc73761b4609fc85edeca
0ac_raf0dev <acennadi@student.1337.ma>
2025-04-04 16:03:08 +0000

    Update

--- includes/minitalk.h
+++ includes/minitalk.h
@@ -1,6 +1,13 @@
 #ifndef MINITALK_H
 #define MINITAL_H
 
+//includes
+#include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
 
+//functions
+void	ft_putnbr_fd(int n, int fd);
+size_t	ft_strlen(const char *s);
 
 #endif 
--- src/utils.c
+++ src/utils.c
@@ -1,12 +1,45 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   client.c                                           :+:      :+:    :+:   */
+/*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: acennadi <acennadi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/04 16:03:04 by acennadi          #+#    #+#             */
-/*   Updated: 2025/04/04 16:03:05 by acennadi         ###   ########.fr       */
+/*   Created: 2025/04/04 16:00:44 by acennadi          #+#    #+#             */
+/*   Updated: 2025/04/04 16:02:48 by acennadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minitalk.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	unsigned int	nb;
+
+	nb = n;
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		nb = -nb;
+	}
+	if (nb < 10)
+	{
+		ft_putchar_fd(nb + '0', fd);
+	}
+	else
+	{
+		ft_putnbr_fd(nb / 10, fd);
+		ft_putnbr_fd(nb % 10, fd);
+	}
+}
