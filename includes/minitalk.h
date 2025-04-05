70630ec5fa4f850daa567327b45b70b749cd6acb
0ac_raf0dev <acennadi@student.1337.ma>
2025-04-05 12:24:21 +0000

    Done

--- includes/minitalk.h
+++ includes/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: acennadi <acennadi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 11:18:52 by acennadi          #+#    #+#             */
-/*   Updated: 2025/04/05 11:18:53 by acennadi         ###   ########.fr       */
+/*   Updated: 2025/04/05 11:42:14 by acennadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 # include <signal.h>
 # include <stdlib.h>
 # include <unistd.h>
+# include <stdio.h>
 
 // functions
 void	ft_putnbr_fd(int n, int fd);
--- src/server.c
+++ src/server.c
@@ -6,17 +6,31 @@
 /*   By: acennadi <acennadi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 16:03:02 by acennadi          #+#    #+#             */
-/*   Updated: 2025/04/05 11:36:23 by acennadi         ###   ########.fr       */
+/*   Updated: 2025/04/05 11:47:27 by acennadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minitalk.h"
 
+void	signal_handler(int signum)
+{
+	if (signum == SIGUSR1)
+		write(1, "0", 1);
+	if (signum == SIGUSR2)
+		write(1, "1", 1);
+	write(1, "\n", 1);
+}
+
 int	main(void)
 {
-	ft_putstr_fd("Welcom to acennadi server PID number : ", 1);
-	ft_putnbr_fd(getpid(), 1);
+	struct sigaction	signal_received;
+
+	signal_received.sa_handler = signal_handler;
+	signal_received.sa_flags = 0;
+	sigemptyset(&signal_received.sa_mask);
+	sigaction(SIGUSR1, &signal_received, NULL);
+	sigaction(SIGUSR2, &signal_received, NULL);
+	printf("Server PID: %d\n", getpid());
 	while (1)
 		usleep(50);
-	return (0);
 }
