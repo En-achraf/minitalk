2aff71c89ec4bc181b3bae54499cb4582c096d06
0ac_raf0dev <acennadi@student.1337.ma>
2025-04-05 11:18:08 +0000

    client Done

--- src/client.c
+++ src/client.c
@@ -6,34 +6,37 @@
 /*   By: acennadi <acennadi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 16:03:04 by acennadi          #+#    #+#             */
-/*   Updated: 2025/04/05 11:12:39 by acennadi         ###   ########.fr       */
+/*   Updated: 2025/04/05 11:17:46 by acennadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minitalk.h"
 
-void    send_signal(char *message, int id_server)
+void	send_signal(char *message, int id_server)
 {
-    int i;
-	int letter;
+	int	i;
+	int	letter;
 
 	i = 0;
 	letter = 0;
-	while (message[letter]) {
+	while (message[letter])
+	{
 		while (i < 8)
 		{
-			if((unsigned int)(message[letter] >> (i - 7)) & 1 == 0)
+			if ((unsigned int)(message[letter] >> (i - 7)) & 1 == 0)
 				kill(id_server, SIGUSR1);
-			if((unsigned int)(message[letter] >> (i - 7)) & 1 == 1)
+			if ((unsigned int)(message[letter] >> (i - 7)) & 1 == 1)
 				kill(id_server, SIGUSR2);
+			usleep(50);
 			i++;
 		}
 		letter++;
 	}
 	i = 0;
-	while (i < 8) 
+	while (i < 8)
 	{
 		kill(id_server, SIGUSR1);
+		usleep(50);
 		i++;
 	}
 }
@@ -52,6 +55,6 @@ int	main(int ac, char **av)
 	message = av[2];
 	if (!pid_num || message[0] == 0)
 		return (ft_putstr_fd("Error: wrong Arg", 1), 1);
-    send_signal(message, pid_num);
+	send_signal(message, pid_num);
 	return (0);
 }
