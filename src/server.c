04e53efa23b278a16ec362c4b02cc67da83d97bb
0ac_raf0dev <acennadi@student.1337.ma>
2025-04-05 11:36:46 +0000

    Server update

--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 /*   By: acennadi <acennadi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 16:03:02 by acennadi          #+#    #+#             */
-/*   Updated: 2025/04/05 11:32:52 by acennadi         ###   ########.fr       */
+/*   Updated: 2025/04/05 11:36:23 by acennadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,9 @@
 
 int	main(void)
 {
-	ft_putstr_fd("PID number : ", 1);
+	ft_putstr_fd("Welcom to acennadi server PID number : ", 1);
 	ft_putnbr_fd(getpid(), 1);
-	
-	while(1)
+	while (1)
 		usleep(50);
 	return (0);
 }
